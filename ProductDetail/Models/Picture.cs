1fce0420f07788e00107b1d3b0c90f3e7c4d0126
01Nijat <mr.nijat.asgarov@gmail.com>
2023-01-18 02:42:23 +0400

    Hazir

---
+++ ProductDetail/Models/Picture.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductDetail.Models
+{
+    public class Picture
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string ImgUrl { get; set; }
+        public int ProductId { get; set; }
+        public virtual Product Product { get; set; }
+    }
+}
---
+++ ProductDetail/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductDetail.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Picture> Pictures { get; set; }
+    }
+}
